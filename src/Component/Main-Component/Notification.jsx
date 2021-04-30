053913c024f8b0dc150c6bfda2c97c4c9453ec20
aakashsh1999 <aakash.sh858791@gmail.com>
2021-05-01 02:50:30 +0530

    login done

--- src/Component/Main-Component/Notification.jsx
+++ src/Component/Main-Component/Notification.jsx
@@ -4,17 +4,12 @@ import '../../Sass/Sass-Main/_home.scss';
 
 
 const Notification = () => {
-  const [show,setShow]=useState(true)
- 
+  const [show,setShow]=useState(true);
+   
   return(
         <>
-
-
-<div className="banner3">
-    
-    
+      <div className="banner3">
         <img src="Assets/images/covid-19.png"/>
-          
           <div className="pop_content">
             <h6>Covid-19 Guidelines & Updates</h6>
             <p>
@@ -36,9 +31,6 @@ const Notification = () => {
                 >
                   <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zm-6.106 4.5L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
                 </svg></button>
-            
-    
-
             </div>
             <button className="same-btn" 
             style={{padding:'0'}}>
