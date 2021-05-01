60137106cee86ef70d5ed42405c2e1f03eb1de92
abhigyan-cloudstok <abhigyan.sharma@cloudstok.com>
2021-05-01 15:13:39 +0530

    edited

--- src/Component/Stepper/stepper.js
+++ src/Component/Stepper/stepper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Step } from 'semantic-ui-react';
 import '../../Sass/app.scss';
 
@@ -24,17 +25,17 @@ const Stepper = () => {
     },[]);
     if(!result){
     return "loading";}
-    
+    arr.pop('/success');
     return (
         <div className="stepper">
             <Step.Group size='tiny' stackable='tablet' widths="6">
                 {result.map((ele, index) => (
-                    <Step active={ele.status} >
+                   <Link to={arr[index]}> <Step active={ele.status} >
                         <Step.Content>
                             <Step.Title>{index + 1}</Step.Title>
                             <Step.Description>{ele.title}</Step.Description>
                         </Step.Content>
-                    </Step>))}
+                    </Step></Link>))}
 
             </Step.Group>
         </div>
