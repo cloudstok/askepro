0532ff4ba94dce6469dac8efe4fe03fcac8e5e68
mohit <mohithrautela@gmail.com>
2021-05-16 03:37:31 +0530

    admin

--- src/Pages/Main/Admin_dashboard.jsx
+++ src/Pages/Main/Admin_dashboard.jsx
@@ -8,7 +8,8 @@ import "../../Sass/Sass-Main/_Request_file.scss";
 import SideBar from "../../Component/Nav/Sidebar";
 import Reject from "../../Component/Main-Component/Reject";
 import Accept from "../../Component/Main-Component/Accept";
-import { Container, Grid, Icon} from "semantic-ui-react";
+import { Container, Grid, Icon } from "semantic-ui-react";
+import Graph_div from '../../Component/Main-Component/Graph_div';
 import "../../Sass/Sass-Main/_Appointment_card.scss";
 import "../../Sass/Sass-Main/_Admin_dashboard.scss";
 import {
@@ -19,7 +20,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer
+  ResponsiveContainer,
 } from "recharts";
 import Verification from "../../Component/Main-Component/Verification";
 import { useHistory } from "react-router";
@@ -27,7 +28,7 @@ import { Link, NavLink } from "react-router-dom";
 
 const Admin_dashboard = () => {
   const history = useHistory();
-    if (!localStorage.getItem("token") && !localStorage.getItem("id"))
+  if (!localStorage.getItem("token") && !localStorage.getItem("id"))
     history.push("/login");
   const [data, setData] = React.useState("null");
   React.useEffect(() => {
@@ -35,19 +36,15 @@ const Admin_dashboard = () => {
   }, []);
   let id = localStorage.getItem("id");
   const getData = async () => {
-
     let result = await (
-      await fetch(
-        `${process.env.REACT_APP_BASE_URL}/admin/dashboard`,
-        {
-          method: "GET",
-        }
-      )
+      await fetch(`${process.env.REACT_APP_BASE_URL}/admin/dashboard`, {
+        method: "GET",
+      })
     ).json();
     setData(result);
-  }
-  if(!data){
-    return <div/>
+  };
+  if (!data) {
+    return <div />;
   }
   console.log(data);
   return (
@@ -89,29 +86,54 @@ const Admin_dashboard = () => {
               </Grid>
               <Grid>
                 <Grid.Column>
-                  {/* <div className="overview5">
+                  <Graph_div />
+            {/*   <div className="overview5">
                     <p>Revenue Overview</p>
-                  </div>
-                  <div className="revenue_data">asd</div> */}
+                  </div> */}
+                    {/*   <div className="revenue_data">
+                      <Grid doubling columns={3}>
+                <Grid.Row>
+                  <Grid.Column>
+                    <div className="Revenue">
+                      <h6>Revenue Overview</h6>
+              
+                    </div>
+                  </Grid.Column>
+                  <Grid.Column>
+                  <div className="Revenue">
+                      <h6>Revenue Overview</h6>
+              
+                    </div>
+                  </Grid.Column>
+                  <Grid.Column>
+                  <div className="Revenue">
+                     <a href="https://google.com"> <h6>Revenue Overview</h6></a>
+              
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+
+                      </div> */}
                   <ResponsiveContainer width="95%" height={400}>
-                  <BarChart
-                    width={875}
-                    height={500}
-                    data={data.graphData}
-                    margin={{
-                      top: 5,
-                      right: 0,
-                      left: 0,
-                      bottom: 5
-                    }}
-                  >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="_id" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="count" fill="#9d9494" />
-                  </BarChart>
+                    <BarChart
+                      width={875}
+                      height={500}
+                      data={data.graphData}
+                      margin={{
+                        top: 5,
+                        right: 0,
+                        left: 0,
+                        bottom: 5,
+                      }}
+                    >
+                      <CartesianGrid strokeDasharray="3 3" />
+                      <XAxis dataKey="_id" />
+                      <YAxis />
+                      <Tooltip />
+                      <Legend />
+                      <Bar dataKey="count" fill="#9d9494" />
+                    </BarChart>
                   </ResponsiveContainer>
                 </Grid.Column>
               </Grid>
@@ -121,12 +143,16 @@ const Admin_dashboard = () => {
               <div className="appointment_heading2">
                 <Grid columns>
                   <Grid.Row>
-                    <Grid.Column width={8}>
-                      <p> Appointments for approval({data.pendingAppointmentCount})</p>
+                    <Grid.Column width={12}>
+                      <p>
+                        {" "}
+                        Appointments for approval({data.pendingAppointmentCount}
+                        )
+                      </p>
                     </Grid.Column>
-                    <Grid.Column floated="right" width={3}>
-                    <Link to="/admin/appointment"> 
-                     <p className="para9"> VIEW ALL</p> 
+                    <Grid.Column floated="right" width={4}>
+                      <Link to="/admin/appointment">
+                        <p className="para9"> VIEW ALL</p>
                       </Link>
                     </Grid.Column>
                   </Grid.Row>
@@ -134,26 +160,36 @@ const Admin_dashboard = () => {
               </div>
               <Grid columns>
                 <Grid.Column>
-                {data.pendingAppointment&&data.pendingAppointment.map((ele) => <div className="border_appointment">
-                    <Grid columns>
-                      <Grid.Column>
-                        <div className="appoint_card">
-                          <div className="appoint_card content">
-                            <div className="content6">
-                              <p>{
-                                ele.title}
-                  </p>
-                              <span className="date_time">{ele.appt_date} {ele.appt_month} {ele.appt_year}</span>
-                            </div>
-                          </div>
-                          <div className="action-icon">
-                            <Reject id={ele._id} />
-                            <Accept id={ele._id} name={ele.users.name} userId={ele.users._id}/>
-                          </div>
+                  <div className="appoint_wrapper">
+                    {data.pendingAppointment &&
+                      data.pendingAppointment.map((ele) => (
+                        <div className="border_appointment">
+                          <Grid columns>
+                            <Grid.Column>
+                              <div className="appoint_card">
+                                <div className="appoint_card content">
+                                  <div className="content6">
+                                    <p>{ele.title}</p>
+                                    <span className="date_time">
+                                      {ele.appt_date} {ele.appt_month}{" "}
+                                      {ele.appt_year}
+                                    </span>
+                                  </div>
+                                </div>
+                                <div className="action-icon">
+                                  <Reject id={ele._id} />
+                                  <Accept
+                                    id={ele._id}
+                                    name={ele.users.name}
+                                    userId={ele.users._id}
+                                  />
+                                </div>
+                              </div>
+                            </Grid.Column>
+                          </Grid>
                         </div>
-                      </Grid.Column>
-                    </Grid>
-                  </div>)}
+                      ))}
+                  </div>
                 </Grid.Column>
               </Grid>
 
@@ -162,10 +198,13 @@ const Admin_dashboard = () => {
                   <div className="appointment_heading3">
                     <Grid columns>
                       <Grid.Row>
-                        <Grid.Column width={8}>
-                          <p>Document Verification Request({data.pendingDcoumentCount})</p>
+                        <Grid.Column width={12}>
+                          <p>
+                            Document Verification Request(
+                            {data.pendingDcoumentCount})
+                          </p>
                         </Grid.Column>
-                        {/* <Grid.Column floated="right" width={3}>
+                        {/* <Grid.Column floated="right" width={4}>
                           <a href={URL}>
                             <p className="para9"> VIEW ALL</p>
                           </a>
@@ -173,35 +212,51 @@ const Admin_dashboard = () => {
                       </Grid.Row>
                     </Grid>
                   </div>
-                  {data.pendingDcoument&&data.pendingDcoument.map((ele) =><div className="Request_bg">
-     <Grid columns>
-     { ele.appointment?   <Grid.Column>
-            <div className="request3">
-              <p>
-                {ele && ele.appointment.title}
-              </p>
-              <Grid column={2}>
-                <Grid.Row>
-                  <Grid.Column width={8}>
-                    <span className="date_time">
-                    {ele &&ele.appointment.appt_date} {ele &&ele.appointment.appt_month} {ele &&ele.appointment.appt_year} 
-                    </span>
-                  </Grid.Column>
-                  <Grid.Column floated="right" width={4}>
-                    <Verification serviceCategory={ele&& ele.serviceCategory.name}
-                    id={ele&& ele._id}
-                    username={ele&& ele.users.name }
-                    email={ele&& ele.users.email }
-                    phone={ele&& ele.users.phone }
-                    time={ele&& new Date(ele.createdAt).toLocaleString()}
-                    docs={ele&& ele.docs} ></Verification>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </div>
-          </Grid.Column>:<br/>}
-        </Grid>
-      </div>)}
+                  {data.pendingDcoument &&
+                    data.pendingDcoument.map((ele) => (
+                      <div className="Request_bg">
+                        <Grid columns>
+                          {ele.appointment ? (
+                            <Grid.Column>
+                              <div className="request3">
+                                <p>{ele && ele.appointment.title}</p>
+                                <Grid column={2}>
+                                  <Grid.Row>
+                                    <Grid.Column width={8}>
+                                      <span className="date_time">
+                                        {ele && ele.appointment.appt_date}{" "}
+                                        {ele && ele.appointment.appt_month}{" "}
+                                        {ele && ele.appointment.appt_year}
+                                      </span>
+                                    </Grid.Column>
+                                    <Grid.Column floated="right" width={4}>
+                                      <Verification
+                                        serviceCategory={
+                                          ele && ele.serviceCategory.name
+                                        }
+                                        id={ele && ele._id}
+                                        username={ele && ele.users.name}
+                                        email={ele && ele.users.email}
+                                        phone={ele && ele.users.phone}
+                                        time={
+                                          ele &&
+                                          new Date(
+                                            ele.createdAt
+                                          ).toLocaleString()
+                                        }
+                                        docs={ele && ele.docs}
+                                      ></Verification>
+                                    </Grid.Column>
+                                  </Grid.Row>
+                                </Grid>
+                              </div>
+                            </Grid.Column>
+                          ) : (
+                            <br />
+                          )}
+                        </Grid>
+                      </div>
+                    ))}
                 </Grid.Column>
               </Grid>
             </Grid.Column>
