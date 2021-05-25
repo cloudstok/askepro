dbfa9c2dd374b425fd354aac2c8bcce7ce6fcc85
abhigyan-cloudstok <abhigyan.sharma@cloudstok.com>
2021-05-25 16:57:35 +0530

    removed static payment

--- src/Pages/Main/Admin_dashboard.jsx
+++ src/Pages/Main/Admin_dashboard.jsx
@@ -69,7 +69,7 @@ const Admin_dashboard = () => {
                   <Grid.Column>
                     <div className="clients">
                       <h6>Total Revenue(in AED)</h6>
-                      <h1>1,00,443.45</h1>
+                      <h1>0</h1>
                     </div>
                   </Grid.Column>
                 </Grid.Row>
