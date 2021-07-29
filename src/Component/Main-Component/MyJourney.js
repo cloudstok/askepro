53b9c1f4d532b31f62c7731e13b920ff0ef76860
ritikagoyal22 <ritika.goyal@cloudstok.com>
2021-07-29 22:56:34 +0530

    home and my journey

--- src/Component/Main-Component/MyJourney.js
+++ src/Component/Main-Component/MyJourney.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Image } from "semantic-ui-react";
 import "../../Sass/Sass-Main/_About.scss"
 
 export const MyJourney = () => {
@@ -7,21 +7,19 @@ export const MyJourney = () => {
     <div>
       <div className="my_journey">
         <h1 className="headingOne">Our Journey </h1>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column className="journey_list">
-              <div className="odd_year">
+        
+              <div className="journey_tree">
                 <Year number='2002'/>
-              </div>
+                {/* <img src={"/Assets/images/service2.png"}/> */}
+                {/* <div className="flex_content">
+                  <h1>Lorem Ipsum</h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing</p> */}
+                {/* </div> */}
+              
               <Year number='2003'/>
               <Year number='2004'/>
               <Year number='2005'/>
-              <Year />
-              <Year />
-              <Year />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+              </div>
       </div>
     </div>
   );
