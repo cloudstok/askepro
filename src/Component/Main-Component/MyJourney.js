85dadfa8d4a6971eb3493a13a8759daef9662613
ritikagoyal22 <ritika.goyal@cloudstok.com>
2021-07-30 14:30:04 +0530

    flow chart

--- src/Component/Main-Component/MyJourney.js
+++ src/Component/Main-Component/MyJourney.js
@@ -7,19 +7,52 @@ export const MyJourney = () => {
     <div>
       <div className="my_journey">
         <h1 className="headingOne">Our Journey </h1>
-        
-              <div className="journey_tree">
-                <Year number='2002'/>
-                {/* <img src={"/Assets/images/service2.png"}/> */}
-                {/* <div className="flex_content">
-                  <h1>Lorem Ipsum</h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing</p> */}
-                {/* </div> */}
-              
-              <Year number='2003'/>
-              <Year number='2004'/>
-              <Year number='2005'/>
+        <div className="journey_tree">
+
+          <div className="odd_child">
+            <Year number='2002' />
+            <div className="odd_child_content">
+              <img src={"/Assets/images/login-bg.png"} />
+              <div className="flex_content">
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing</p>
               </div>
+            </div>
+          </div>
+
+          <div className="even_child">
+            <Year number='2003' />
+            <div className="even_child_content">
+              <div className="flex_content">
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing</p>
+              </div>
+              <img src={"/Assets/images/login-bg.png"} />
+            </div>
+          </div>
+
+          <div className="odd_child">
+            <Year number='2004' />
+            <div className="odd_child_content">
+              <img src={"/Assets/images/login-bg.png"} />
+              <div className="flex_content">
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing</p>
+              </div>
+            </div>
+          </div>
+          
+          <div className="even_child">
+            <Year number='2005' />
+            <div className="even_child_content">
+              <div className="flex_content">
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing</p>
+              </div>
+              <img src={"/Assets/images/login-bg.png"} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
