da31a048a4ff1361ab52efcba2fc7c21f3f9569f
Mohit-Rautela <mohit.rautela@cloudstok.com>
2021-08-02 13:16:47 +0530

    effect delay

--- src/Component/Main-Component/MyJourney.js
+++ src/Component/Main-Component/MyJourney.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Grid, Image } from "semantic-ui-react";
 import "../../Sass/Sass-Main/_About.scss"
+import AOS from "aos";
 
 export const MyJourney = () => {
+  AOS.init({
+    offset: 100,
+    duration: 1100,  
+    easing: 'ease-in-sine',
+    delay: 100, 
+  });
   return (
     <div>
       <div className="my_journey">
         <h1 className="headingOne">Our Journey </h1>
         <div className="journey_tree">
 
-          <div className="odd_child">
+          <div className="odd_child" >
             <Year number='2002' />
-            <div className="odd_child_content">
+            <div className="odd_child_content" data-aos="fade-right">
               <img src={"/Assets/images/login-bg.png"} />
               <div className="flex_content">
                 <h1>Lorem Ipsum</h1>
@@ -20,10 +27,10 @@ export const MyJourney = () => {
             </div>
           </div>
 
-          <div className="even_child">
+          <div className="even_child"  >
             <Year number='2003' />
-            <div className="even_child_content">
-              <div className="flex_content">
+            <div className="even_child_content"data-aos="fade-left" >
+              <div className="flex_content" >
                 <h1>Lorem Ipsum</h1>
                 <p>Lorem Ipsum is simply dummy text of the printing</p>
               </div>
@@ -31,9 +38,9 @@ export const MyJourney = () => {
             </div>
           </div>
 
-          <div className="odd_child">
+          <div className="odd_child" >
             <Year number='2004' />
-            <div className="odd_child_content">
+            <div className="odd_child_content" data-aos="fade-right"> 
               <img src={"/Assets/images/login-bg.png"} />
               <div className="flex_content">
                 <h1>Lorem Ipsum</h1>
@@ -42,9 +49,9 @@ export const MyJourney = () => {
             </div>
           </div>
           
-          <div className="even_child">
+          <div className="even_child"  >
             <Year number='2005' />
-            <div className="even_child_content">
+            <div className="even_child_content" data-aos="fade-left">
               <div className="flex_content">
                 <h1>Lorem Ipsum</h1>
                 <p>Lorem Ipsum is simply dummy text of the printing</p>
